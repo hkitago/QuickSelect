dfff82640ab30fd31d919cb84e337fac63ede5b7
hkitago <hkitago@gmail.com>
2026-02-07 11:42:09 +0900

    Update templates for OS-side application display

--- Shared (App)/ViewController.swift
+++ Shared (App)/ViewController.swift
@@ -25,6 +25,11 @@ class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMess
     override func viewDidLoad() {
         super.viewDidLoad()
 
+#if os(macOS)
+      webView.setValue(false, forKey: "drawsBackground")
+      webView.setValue(NSColor.clear, forKey: "backgroundColor")
+#endif
+
         self.webView.navigationDelegate = self
 
 #if os(iOS)
@@ -60,22 +65,39 @@ class ViewController: PlatformViewController, WKNavigationDelegate, WKScriptMess
     }
 
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-#if os(macOS)
-        if (message.body as! String != "open-preferences") {
-            return
-        }
+      let messageBody = message.body as! String
+      var url: URL?
 
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
-            guard error == nil else {
-                // Insert code to inform the user that something went wrong.
-                return
-            }
+      if messageBody == "open-support" {
+          url = URL(string: "https://github.com/hkitago/QuickSelect/")
+      }
 
-            DispatchQueue.main.async {
-                NSApp.terminate(self)
-            }
-        }
-#endif
-    }
+  #if os(macOS)
+      if messageBody == "open-preferences" {
+          SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+              guard error == nil else {
+                  return
+              }
+              DispatchQueue.main.async {
+                  NSApp.terminate(self)
+              }
+          }
+          return
+      }
+
+      if let validURL = url {
+          NSWorkspace.shared.open(validURL)
+      }
+
+  #elseif os(iOS)
+      if messageBody == "open-settings" {
+          url = URL(string: UIApplication.openSettingsURLString)
+      }
+
+      if let validURL = url {
+          UIApplication.shared.open(validURL)
+      }
+  #endif
+  }
 
 }
